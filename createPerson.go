1d35e69f62ff39d599fb3a06a10361ca13d5933c
Pourya <cepsociety@gmail.com>
2022-04-19 00:30:34 +0300

    Create createPerson.go

---
+++ createPerson.go
@@ -0,0 +1,11 @@
+
+func createPerson(w http.ResponseWriter, r *http.Request) {
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var person entity.Person
+	json.Unmarshal(requestBody, &person)
+
+	database.Connector.Create(person)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(person)
+}
