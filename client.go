b85087157d4fdb626d4b424f5431c0fb2a9f9484
Pourya <cepsociety@gmail.com>
2022-04-19 00:29:15 +0300

    Create client.go

---
+++ client.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+//Connector variable used for CRUD operation's
+var Connector *gorm.DB
+
+//Connect creates MySQL connection
+func Connect(connectionString string) error {
+	var err error
+	Connector, err = gorm.Open("mysql", connectionString)
+	if err != nil {
+		return err
+	}
+	log.Println("Connection was successful!!")
+	return nil
+}
